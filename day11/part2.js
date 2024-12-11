d9dfba0d1b2b3923ecc68e606092aa1da7c795d0
Maarten Stoelhorst <maarten.stoelhorst@mybit.nl>
2024-12-11 11:31:33 +0100

    day11

---
+++ day11/part2.js
@@ -0,0 +1,39 @@
+import {readFileSync} from "fs";
+
+let numbers = readFileSync('input.txt').toString('utf8').trimEnd().split(' ').reduce((acc, val) => {
+    if (!!acc[val]) acc[val]++;
+    else acc[val] = 1;
+
+    return acc;
+}, {});
+
+const blinks = 75;
+
+function process(state) {
+    const newState = {};
+
+    Object.entries(state).forEach(([number, amount]) => {
+        const numberLength = number.toString().length;
+
+        if (number === '0') {
+            newState['1'] = amount;
+        } else if (numberLength % 2 === 0) {
+            const firstHalf = +number.toString().substring(0, numberLength / 2);
+            const secondHalf = +number.toString().substring(numberLength / 2)
+
+            !!newState[firstHalf] ? newState[firstHalf] += amount : newState[firstHalf] = amount;
+            !!newState[secondHalf] ? newState[secondHalf] += amount : newState[secondHalf] = amount;
+        } else {
+            const multiple = (+number * 2024).toString();
+            !!newState[multiple] ? newState[multiple] += amount : newState[multiple] = amount;
+        }
+    });
+
+    return newState;
+}
+
+for (let i = 0; i < blinks; i++) {
+    numbers = process(numbers);
+}
+
+console.log(Object.values(numbers).reduce((acc, val) => acc += val, 0));
